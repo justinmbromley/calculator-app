dca5148a4c85350d86f9dee136005e5ab6963dba
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-08-23 19:32:40 +1000

    Implemented the Calculator class

--- src/calculator.py
+++ src/calculator.py
@@ -1,12 +1,12 @@
 class Calculator:
     def plus(num_1: int, num_2: int) -> int:
-        return None
+        return num_1 + num_2
     
     def subtract(num_1: int, num_2: int) -> int:
-        return None
+        return num_1 - num_2
     
     def product(num_1: int, num_2: int) -> int:
-        return None
+        return num_1 * num_2
     
     def quotient(num_1: int, num_2: int) -> int:
-        return None
+        return num_1 / num_2
